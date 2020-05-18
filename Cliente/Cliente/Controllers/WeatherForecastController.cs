b10958b72a205b83b2bc642d703d1af097a31a1f
renatomarcio18081971 <renato.marcio@gmail.com>
2020-05-18 14:40:41 -0300

    corrigido swagger

--- Cliente/Cliente/Controllers/WeatherForecastController.cs
+++ Cliente/Cliente/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,10 @@ using System.Linq;
 
 namespace Cliente.Controllers
 {
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
